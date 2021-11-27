a72d79907d033454872f72fc8e54b625361ceffa
Riley <increaseblue666@gmail.com>
2021-11-27 22:26:15 +0900

    Modify : userTire 리턴타입 수정 (#4)

--- src/usertires/usertires.controller.ts
+++ src/usertires/usertires.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserTireDto } from './dto/create-usertire.dto';
+import { UserTire } from './entities/usertire.entity';
 import { UserTiresService } from './usertires.service';
 
 @Controller('usertires')
@@ -7,12 +8,16 @@ export class UserTiresController {
   constructor(private userTiresService: UserTiresService) {}
 
   @Post()
-  create(@Body() createUserTireDto: CreateUserTireDto[]) {
+  create(
+    @Body() createUserTireDto: CreateUserTireDto[],
+  ): Promise<{ message: string }> {
     return this.userTiresService.create(createUserTireDto);
   }
 
   @Get(':id')
-  findTireByUserId(@Param('id') id: string) {
+  findTireByUserId(
+    @Param('id') id: string,
+  ): Promise<{ message: string; tire: UserTire[] }> {
     return this.userTiresService.findTireByUserId(id);
   }
 }
