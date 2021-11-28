b4f4ded120414810f8160f45f20725e9f490cc97
Riley <increaseblue666@gmail.com>
2021-11-28 14:34:53 +0900

    Doc : Swagger 적용 (#6)
    - dto 등록
    - 각 api 설명 작성
    - response example 작성

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const option = new DocumentBuilder()
+    .setTitle('Cardoc Assignment')
+    .setDescription('원티드x위코드 백엔드 프리온보딩 과제 7 : 카닥')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, option);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
